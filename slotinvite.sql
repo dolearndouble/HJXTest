2ab25bbdb63030dc9e58c3226dc67b4602b2458f
hejianxiong <2284090522@qq.com>
2024-09-09 19:06:22 +0800

    加表

--- slotinvite.sql
+++ slotinvite.sql
@@ -19,19 +19,35 @@
 CREATE DATABASE IF NOT EXISTS `slotinvite` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `slotinvite`;
 
+-- 导出  表 slotinvite.daylys 结构
+CREATE TABLE IF NOT EXISTS `daylys` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uid` bigint DEFAULT NULL,
+  `lasttime` bigint DEFAULT (0),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 正在导出表  slotinvite.daylys 的数据：~0 rows (大约)
+DELETE FROM `daylys`;
+INSERT INTO `daylys` (`id`, `uid`, `lasttime`) VALUES
+	(2, 795177, 1725878540);
+
 -- 导出  表 slotinvite.invites 结构
 CREATE TABLE IF NOT EXISTS `invites` (
   `id` int NOT NULL AUTO_INCREMENT,
   `parentcode` varchar(50) NOT NULL DEFAULT '0',
   `childcode` varchar(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 正在导出表  slotinvite.invites 的数据：~1 rows (大约)
+-- 正在导出表  slotinvite.invites 的数据：~5 rows (大约)
 DELETE FROM `invites`;
 INSERT INTO `invites` (`id`, `parentcode`, `childcode`) VALUES
 	(1, '795176', '333333'),
-	(5, '795177', '333334');
+	(5, '795177', '333334'),
+	(6, '795177', '111111'),
+	(7, '795177', '123456'),
+	(8, '795177', '795177');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
